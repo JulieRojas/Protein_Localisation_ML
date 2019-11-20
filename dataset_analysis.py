bf37585bf7944a5504a56808e71a0ab9e0422321
Unknown <julie.cm.rojas@gmail.com>
2019-11-21 00:31:03 +0100

    Jupyter notebook data analysis, test and train set created.

--- dataset_analysis.py
+++
@@ -1,8 +0,0 @@
-import matplotlib
-import pandas as pd
-import csv
-
-df = pd.read_csv('data/Dataset.csv', sep="\t")
-df.head()
-print(df.columns)
-df.groupby("Compartment Prediction").count().hist()
